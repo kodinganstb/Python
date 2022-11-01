542eeb6c07b6c9d294600530fee54a415a5614b0
Asifa Ahmad <97321673+Lushiva@users.noreply.github.com>
2022-11-01 09:15:33 +0700

    Add files via upload

---
+++ Dasar Python/intro_python.py
@@ -0,0 +1,27 @@
+# Introduction
+print(2 + 3)   # addition(+)                    |penjumlahan|
+print(3 - 1)   # subtraction(-)                 |pengurangan|
+print(2 * 3)   # multiplication(*)              |perkalian|
+print(3 / 2)   # division(/)                    |pembagian|
+print(3 ** 2)  # exponential(**)                |perpangkatan|
+print(3 % 2)   # modulus(%)                     |sisa bagi|
+print(3 // 2)  # Floor division operator(//)    |pembulatan hasil bagi|
+
+# Tipe Data Pada Python
+a=(10)                  # Int
+b=(3.14)                # Float
+c=(1+3j)                # Complex
+d=('stimik')            # String
+e=([1,2,3])             # List
+f=({'name':'stimik'})   # Dictionary
+g=({9.8,3.14,2.7})      # Set
+h=((9.8,3.14,2.7))      # Tuple
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
